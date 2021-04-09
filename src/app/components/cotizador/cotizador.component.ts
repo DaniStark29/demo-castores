a5babd061229048d00d856adfee489f76b969b02
Daniel <danistark29@gmail.com>
2021-04-08 19:59:28 -0500

    Calcular get

--- src/app/components/cotizador/cotizador.component.ts
+++ src/app/components/cotizador/cotizador.component.ts
@@ -26,22 +26,48 @@ export class CotizadorComponent implements OnInit {
 
         });
     
-    this.apiS.getempaques()
-    .subscribe( resp => {
+    this.apiS.getempaques().subscribe( resp => {
 
       console.log(resp);
 
     });
-
+    
+    this.formCotizador();
+    
   }
 
   formCotizador() {
-
+  
     const data = {
-      id: '12',
-      userId: '1',
-      title: 'change title',
-      completed: true
+      "valordeclarado":1000,
+      "valor":16,
+      "prodquimico":0,
+      "idmoneda":1,
+      "ocurre":false,
+      "otros":0,
+      "recoleccion":0,
+      "idconvenio":0,
+      "completo":0,
+      "idtipounidad":1,
+      "mensajeria":1,
+      "idsucursal":1101,
+      "blnCodigo":false,
+      "porcCodigo":0,
+      "admonocurre":0,
+      "tcliente":0,
+      "cantidad": "1",
+      "idempaque": "1001",
+      "contiene": "Bolsas",
+      "peso": "00010",
+      "mts3": "00040",
+      "etiquetas": "1",
+      "pesoe": "00011",
+      "pesot": "00010",
+      "preciou": "600",
+      "largo": "01500",
+      "ancho": "01500",
+      "alto": "01000",
+      "id": 0
     };
           
     this.apiS.calcula(data)
@@ -50,7 +76,7 @@ export class CotizadorComponent implements OnInit {
       console.log(resp);
 
     });
-    
+
   }
 
 }
--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,14 +1,13 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-
-
+import axios from 'axios';
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
-
+  
   constructor( private http: HttpClient ) { }
-
+  
   getciudadescotizador() {
 
     const url = 'http://localhost:8000/api/origen';
@@ -27,13 +26,31 @@ export class ApiService {
 
     const url = 'http://localhost:8000/api/empaques';
     return this.http.get( url );
+    // return axios.get('http://localhost:8000/api/empaques');
     
   }
 
-  calcula(idorigen: any) {
-
-    const url = `http://localhost:8000/api/calcular/${idorigen}`;
+calcula(data: any) {
+    
+    const url = `http://localhost:8000/api/calcular/${data.valordeclarado}/${data.valor}/${data.prodquimico}/${data.idmoneda}/${data.ocurre}/${data.otros}/${data.recoleccion}/${data.idconvenio}/${data.completo}/${data.idtipounidad}/${data.mensajeria}/${data.idsucursal}/${data.blnCodigo}/${data.porcCodigo}/${data.admonocurre}/${data.tcliente}`;
     return this.http.get( url );
+    // const datos = {
+    //   "cantidad": "1",
+    //   "idempaque": "1001",
+    //   "contiene": "Bolsas",
+    //   "peso": "0.0010",
+    //   "mts3": "00040",
+    //   "etiquetas": "1",
+    //   "pesoe": "00011",
+    //   "pesot": "00010",
+    //   "preciou": "600",
+    //   "largo": "01500",
+    //   "ancho": "01500",
+    //   "alto": "01000",
+    //   "id": 0
+    // };
+
+    // return axios.post('http://localhost:8000/api/calcular2', datos);
     
   }
 
