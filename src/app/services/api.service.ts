343d665c5f7685d550d6b8f216e65ad598aff508
CarolinaAvalos <carolina.v@yocontigo-it.com>
2021-04-20 12:49:50 -0500

    validaciones

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -10,31 +10,28 @@ export class ApiService {
   constructor( private http: HttpClient ) { }
 
   getciudadescotizador() {
-
     const url = 'http://localhost:8000/api/origen';
     return this.http.get( url );
-    
+
   }
 
   getdestinos() {
 
     const url = 'http://localhost:8000/api/destinos';
     return this.http.get( url );
-    
+
   }
 
   getempaques() {
 
     const url = 'http://localhost:8000/api/empaques';
     return this.http.get( url );
-    
+
   }
 
 calcula(data: any) {
-    
     const url = `http://localhost:8000/api/calcular/${data.idorigen}/${data.iddestino}/${data.valordeclarado}/${data.valor}/${data.prodquimico}/${data.idmoneda}/${data.ocurre}/${data.otros}/${data.recoleccion}/${data.idconvenio}/${data.completo}/${data.idtipounidad}/${data.mensajeria}/${data.idsucursal}/${data.blnCodigo}/${data.porcCodigo}/${data.admonocurre}/${data.tcliente}/${data.cantidad}/${data.idempaque}/${data.contiene}/${data.peso}/${data.mts3}/${data.etiquetas}/${data.pesoe}/${data.pesot}/${data.preciou}/${data.largo}/${data.ancho}/${data.alto}/${data.id}`;
     return this.http.get( url );
-    
   }
 
 }
